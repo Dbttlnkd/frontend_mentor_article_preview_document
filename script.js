783e7ce420befe5b5b5078f1512978db59447133
Dbttlnkd <108098172+Dbttlnkd@users.noreply.github.com>
2022-07-14 22:50:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const share = document.querySelector('.share')
 const share_container= document.querySelector('.share_container')
+const html_content = document.querySelector("html");
 
 share.addEventListener("click", (e) => {
     console.log(e);
-    share.style.backgroundColor = "#6E8098"
-    share.style.backgroundImage = "url(/images/icon-share_white.svg)";
-    share_container.style.display = "flex"
+    share_container.classList.toggle("show")
+    share.classList.toggle("share_toggle")
 })
+
+// html_content.addEventListener("click", function (e) {
+//     if (e.target !== share)
+//     share.classList.remove("share_toggle")
+// })
+
+// html_content.addEventListener("click", function (e) {
+//     if (e.target !== share_container)
+//     share_container.style.visibility="visible"
+// })
+
