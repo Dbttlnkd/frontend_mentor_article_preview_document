04f3190ce145c08d597b68a366ed015bf13b40d2
Dbttlnkd <108098172+Dbttlnkd@users.noreply.github.com>
2022-07-16 09:10:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const share = document.querySelector('.share')
-const share_container= document.querySelector('.share_container')
-const html_content = document.querySelector("html");
+const share = document.querySelector(".share");
+const share_container = document.querySelector(".share_container");
+const square = document.querySelector(".square");
+const test = document.querySelector("card")
 
-share.addEventListener("click", (e) => {
-    console.log(e);
-    share_container.classList.toggle("show")
-    share.classList.toggle("share_toggle")
+share.addEventListener("click", (ev) => {
+  share_container.classList.toggle("show");
+  square.classList.toggle("show");
+  share.classList.toggle("share_toggle");
+  console.log(ev);
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target !== share) {
+            share.classList.remove("share_toggle")
+            share_container.classList.remove("show");
+            square.classList.remove("show")
+    }
 })
 
-// html_content.addEventListener("click", function (e) {
-//     if (e.target !== share)
-//     share.classList.remove("share_toggle")
-// })
-
-// html_content.addEventListener("click", function (e) {
-//     if (e.target !== share_container)
-//     share_container.style.visibility="visible"
-// })
-
+test.addEventListener('mouseover', (ev) => {
+    console.log(ev)
+})
